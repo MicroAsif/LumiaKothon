8b3ffa2f80bc00f0343fa17e00ac4e861a6ccda3
ASif <asif0531@live.com>
2014-02-03 02:19:04 +0600

    Initial Commit

---
+++ LumiaKothon/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using LumiaKothon.Resources;
+
+namespace LumiaKothon
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
