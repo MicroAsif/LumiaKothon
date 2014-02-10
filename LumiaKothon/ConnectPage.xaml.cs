131db6a77e8cd3a7c1531b9f25aa13c67f1ca9b1
Asif Rahman <asif0531@live.com>
2014-02-09 17:19:47 -0800

    facebook api and classes added

---
+++ LumiaKothon/ConnectPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace LumiaKothon
+{
+    public partial class ConnectPage : PhoneApplicationPage
+    {
+        public ConnectPage()
+        {
+            InitializeComponent();
+        }
+
+        private void WebBrowser_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
